aaa4142627e10702bb411d726d791680678216af
Matheus Oliveira <oliveiramarinho789@gmail.com>
2020-11-17 11:45:53 -0400

    adicionado aula 11

---
+++ 3-LaçosDeRepetição/11-laco-de-repeticao-while.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função principal do programa
+void main(){
+	
+	//Definindo Variáveis
+	int a = 1, b = 10;
+	
+	//Contando até 10
+	while(a <= 10){
+		
+		//Imprimindo 'a' na tela
+		printf("%d\n", a);
+		
+		//Incremento
+		a++; //a = a + 1;
+	}
+	
+	//Contagem Regressiva
+	while(b >= 1){
+		
+		//Imprimindo 'b' na tela
+		printf("%d\n", b);
+		
+		//decremento
+		b--; //b = b - 1;
+	}
+	
+	//Pausa o programa após executar
+	system("pause");
+}
